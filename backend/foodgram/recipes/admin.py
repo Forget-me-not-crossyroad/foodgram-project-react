e7316aa189ed157c09e6bb6ffb6a8d1204ddd3ce
Forget-me-not-crossyroad <milleralex629@gmail.com>
2024-03-18 01:07:00 +0300

    Добавление оставшихся моделей в admin-зону

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import (Ingredient, IngredientRecipe, Tag)
+from .models import (Ingredient, IngredientRecipe, Tag, ShoppingCart, Favorite)
 
 
 class IngredientsInline(admin.TabularInline):
@@ -26,6 +26,7 @@ class IngredientRecipeAdminPanel(admin.ModelAdmin):
     list_filter = ('recipe', 'ingredient')
     search_fields = ('name', 'id')
 
+
 class FavoriteAdminPanel(admin.ModelAdmin):
     list_display = ('favorited_user', 'favorited_recipe')
     list_filter = ('favorited_user', 'favorited_recipe')
@@ -38,8 +39,25 @@ class ShoppingCartAdminPanel(admin.ModelAdmin):
     search_fields = ('shoppingcart_user', 'shoppingcart_recipe')
 
 
-
+class RecipeAdminPanel(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'text',
+        'image'
+        'name',
+        'created',
+        'cooking_time',
+        'ingredients',
+        'tags',
+        'author',
+    )
+    search_fields = ('name', 'cooking_time', 'author',)
+    list_filter = ('name', 'ingredients', 'tags', 'cooking_time', 'author', 'created')
+    inlines = [IngredientsInline]
 
 
 admin.site.register(Ingredient, IngredientAdminPanel)
 admin.site.register(Tag, TagAdminPanel)
+admin.site.register(ShoppingCart, ShoppingCartAdminPanel)
+admin.site.register(Favorite, FavoriteAdminPanel)
+admin.site.register(IngredientRecipe, IngredientRecipeAdminPanel)
